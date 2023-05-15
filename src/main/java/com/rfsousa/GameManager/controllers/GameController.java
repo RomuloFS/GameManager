8a30c48cdeb81bd5ddf065c31957862132946b13
RomuloFS <romulosousa865@gmail.com>
2023-05-14 23:12:17 -0300

    Adicionando GET game by id

--- src/main/java/com/rfsousa/GameManager/controllers/GameController.java
+++ src/main/java/com/rfsousa/GameManager/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rfsousa.GameManager.dto.GameDTO;
 import com.rfsousa.GameManager.dto.GameMinDTO;
 import com.rfsousa.GameManager.services.GameService;
 
@@ -20,5 +22,10 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		return gameService.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		return gameService.findById(id);
 	}
 }
--- src/main/java/com/rfsousa/GameManager/services/GameService.java
+++ src/main/java/com/rfsousa/GameManager/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rfsousa.GameManager.dto.GameDTO;
 import com.rfsousa.GameManager.dto.GameMinDTO;
 import com.rfsousa.GameManager.entities.Game;
 import com.rfsousa.GameManager.repositories.GameRepository;
@@ -15,9 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> gameList = gameRepository.findAll();
-		
 		return gameList.stream().map( x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get();
+		return new GameDTO(game);
 	}
 }
